d0ce1de858dd92c946019248202f353e3e156d6f
Anum Mujahid <amujahid.bese18seecs@student.nust.edu.pk>
2021-05-07 00:57:53 +0500

    feat(login): add spotify login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
-  return (
-    <div className="app">
-      
-    </div>
-  );
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    const _token = hash.access_token;
+    window.location.hash = '';
+    if (_token) {
+      setToken(_token);
+    }
+  });
+  return <div className="app">{token ? <h1>Logged In</h1> : <Login />}</div>;
 }
 
 export default App;
